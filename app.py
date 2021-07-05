b60b2a05c387a98b78e23a38a783788b6f802246
Arpit Somani <arpit.somanias8@gmail.com>
2021-07-05 23:02:34 +0530

    Route and Template of home page

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ app = Flask("car_model")
 # load the ml model which we have saved earlier in .pkl format
 model = pickle.load(open('car_price_model.pkl', 'rb'))
 
+# define the route(basically url) to which we need to send http request
+# HTTP GET request method
+@app.route('/', methods=['GET'])
+# create a function Home that will return index.html(which contains html form)
+# index.html file is created seperately
+def Home():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     # run method starts our web service
